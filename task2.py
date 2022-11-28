c228baae6161160bae001882cc5f45da6cad1358
Valerii <81074936+valerii-chirkov@users.noreply.github.com>
2022-11-28 08:34:17 +0300

    Update task2.py
    Make it into one decorator
    Added __del__ method to print result

--- task2.py
+++ task2.py
@@ -1,63 +1,43 @@
 import time
 import random
-from collections import defaultdict
 from typing import Callable
 
-methods = defaultdict(float)
+
+def speed_test(obj: object) -> object:
+    def wrapper(func: Callable, counter: str) -> Callable:
+        def inner(*args, **kwargs) -> None:
+            t1 = time.time()
+            func(*args, **kwargs)
+            t2 = time.time()
+            counter_value = getattr(obj, counter)
+            counter_value += t2 - t1
+            setattr(obj, counter, counter_value)
+        return inner
+
+    def del_obj(counters: list) -> object:
+        def inner(self) -> object:
+            for counter in counters:
+                print(counter, getattr(self, counter))
+            return obj
+        return inner
+
+    def wrap_obj() -> object:
+        method_list = [fu for fu in dir(obj)
+                       if callable(getattr(obj, fu))
+                       and not fu.startswith('__')]
+        counters_list = [str(method)+'_counter' for method in method_list]
+
+        for pos, method in enumerate(method_list):
+            attr = getattr(obj, method)
+            setattr(obj, counters_list[pos], 0)
+            setattr(obj, method, wrapper(attr, counters_list[pos]))
+        setattr(obj, "__del__", del_obj(counters_list))
+
+        return obj
+    return wrap_obj()
 
 
-def add_time_tracker(func: type) -> Callable:
-    """
-    Covers method (e.g. class method) and count time of exec
-    :param func: e.g. <function Closed.m1 at 0x102be0670>
-    :return:
-    """
-
-    def wrapper(*args, **kwargs) -> None:
-        t1 = time.time()
-        func(*args, **kwargs)
-        t2 = time.time()
-        res = t2 - t1
-        methods[func] += res
-
-    return wrapper
-
-
-def funcs_tracker(extra_functionality: Callable) -> Callable:
-    """
-    Covers a class to expand its functionality but not to modify the class
-    :param extra_functionality: tuple of decorators
-    :return: a new instance of a class with expanded functionality
-    """
-    def inner(obj: type) -> Callable:
-        def wrapper(*args, **kwargs) -> Callable:
-            method_list = [func for func in dir(obj)
-                           if callable(getattr(obj, func))
-                           and not func.startswith('__')]
-            for method in method_list:
-                attr = getattr(obj, method)
-                setattr(obj, method, extra_functionality(attr))
-            return obj(*args, **kwargs)
-        return wrapper
-    return inner
-
-
-def get_methods_items(func: type) -> Callable:
-    """
-    Stdout items of 'methods' dictionary
-    :param func:
-    :return: None
-    """
-    def wrapper(*args, **kwargs) -> None:
-        func(*args, **kwargs)
-        for k, v in methods.items():
-            method_name = str(k).split()[1]
-            method_value = round(float(v), 2)
-            print("{}: {}".format(method_name, method_value))
-    return wrapper
-
-
-@funcs_tracker(extra_functionality=add_time_tracker)
+@speed_test
 class Closed(object):
     def m1(self, n) -> None:
         time.sleep(n)
@@ -66,17 +46,13 @@ class Closed(object):
         time.sleep(n)
 
 
-@get_methods_items
 def do_test(obj: Closed) -> None:
     for r in range(50):
         if random.random() > 0.5:
-            obj.m1(random.random() * 0.1)
+            obj.m1(random.random() * 0.3)
         else:
             obj.m2(random.random() * 0.1)
 
 
 obj = Closed()
 do_test(obj)
-
-# Closed.m2: 1.58
-# Closed.m1: 1.42
